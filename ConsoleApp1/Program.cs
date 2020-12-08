8b88b67b9f6d5eb00ad6734f64981c7cf609a5d7
Elvis Serafim <38743740+ElvisSerafim@users.noreply.github.com>
2020-12-08 14:49:43 -0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,31 +1,34 @@
-﻿using System;
+using System;
 using System.Collections;
 using TekVISANet;
+using MySqlConnector;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args) 
+        static async Task Main(string[] args) 
         {
             string response = "";
             Console.WriteLine("Hello World!");
             VISA TVA = new VISA();
             ArrayList resources = new ArrayList();
-            TVA.FindResources("?*", out resources);
+            TVA.FindResources("?*", out resources); //Searching for oscilloscope connected on PC
 
+            //Showing the osciloscope ID
             foreach (string s in resources)
             {
                 Console.WriteLine(s);
             }
 
-            TVA.Open(resources[0].ToString());
+            TVA.Open(resources[0].ToString()); //Connecting with the oscilloscope
             TVA.Write("*IDN?");
             bool status = TVA.Read(out response);
             if (status)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("IDN (device information):");
+                Console.WriteLine("IDN (device information):"); //ID oscilloscope
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine(response);
             }
@@ -38,13 +41,17 @@ namespace ConsoleApp1
             TVA.Write("SEL:REFA ON");
             TVA.Write("DATA:SOU REFA");
 
+            //Setting the wave length that will be stored
             TVA.Write("DATA:ENCDG SRPbinary");
             TVA.Write("DATA:WIDTH 1");
             TVA.Write("DATA:START 1");
             TVA.Write("DATA:STOP 2500");
           
+            //Starting the acquisition data on Oscilloscope
             TVA.Write("ACQ:STOPAFTER SEQUENCE;MODE SAMPLE;STATE RUN;");
             TVA.Write("ACQ:STATE STOP");
+
+            //Saving the data on REFA memory
             TVA.Write("SAVE:WAVE CH2, REFA");
 
             
@@ -72,6 +79,47 @@ namespace ConsoleApp1
             TVA.Write("MEASUREMENT:IMMED:VALUE?");
             TVA.Read(out riseTime);
             Console.WriteLine(riseTime);
+
+            //Setting the oscilloscope for get Vpp(peak-to-peak value)
+            string pkp;
+            TVA.Write("MEASUREMENT:IMMED:TYPE PK2PK");
+            TVA.Write("MEASUREMENT:IMMED:SOURCE CH2");
+            TVA.Write("MEASUREMENT:IMMED:VALUE?");
+            TVA.Read(out pkp);
+            Console.WriteLine(pkp);
+
+            //Setting the Storage information
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database = "detectorlabensol",
+                UserID = "root",
+                Password = "projetoMCA20",
+                SslMode = MySqlSslMode.Required,
+            };
+
+            //Establishing the connection with the Database
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                Console.WriteLine("Opening connection");
+                await conn.OpenAsync();
+
+                using (var command = conn.CreateCommand())
+                {
+
+                    command.CommandText = @"INSERT INTO detection (negativeWitdh, fallTime, riseTime, peakToPeak) VALUES (@name1, @name2, @name3, @name4);"; //Inserting the data on table
+                    command.Parameters.AddWithValue("@name1", nWidth);
+                    command.Parameters.AddWithValue("@name2", fallTime);
+                    command.Parameters.AddWithValue("@name3", riseTime);
+                    command.Parameters.AddWithValue("@name4", pkp);
+
+                    int rowCount = await command.ExecuteNonQueryAsync();
+                    Console.WriteLine(String.Format("Number of rows inserted={0}", rowCount));
+                }
+
+                // connection will be closed by the 'using' block
+                Console.WriteLine("Closing connection");
+            }
 
         }
     }
